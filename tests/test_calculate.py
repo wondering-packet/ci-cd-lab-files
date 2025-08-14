9108039001fda95fd4f64be57f3c61fef968d9f6
wondering-packet <packetwondering@gmail.com>
2025-08-15 04:57:19 +0530

    test

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -2,7 +2,7 @@ from apps.calculator_private import add, divide, multiply, subtract
 
 
 def test_add():
-    assert add(3, 3) == 6
+    assert add(3, 7) == 10
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
     assert multiply(2, 3) == 6
